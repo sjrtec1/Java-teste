973d07c305d65d70f52851c6d7ec58aea19836b4
Silvério Rodrigues <34194242+Silverio57@users.noreply.github.com>
2024-04-17 10:32:26 -0300

    Validação simples do usuario

--- java_teste/Teste.java
+++ java_teste/Teste.java
@@ -11,18 +11,19 @@ public class Teste {
 
     public static void main(String[] args) {
         String senhaCadastrada = "1234";
-        
+        String usuarioCadastrado = "silverio";
+
         System.out.println("Programa em execução...\n");
 
         Scanner t = new Scanner(System.in);
 
-        System.out.println("digite seu nome: ");
-        String nome = t.next();
+        System.out.println("digite seu usuário: ");
+        String usuario = t.next();
 
         System.out.println("Digite sua senha: ");
         String senha = t.next();
 
-        if(senha.equalsIgnoreCase(senhaCadastrada)){
+        if(senha.equalsIgnoreCase(senhaCadastrada) && usuarioCadastrado.equalsIgnoreCase(usuario)){
             System.out.println("Seja bem vindo, "+nome);
 
         }else{
